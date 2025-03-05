2c087cc29ae9d526e2952a1b4d2ab6b9dc71f62e
Laura Thomson <lrthmsn@gmail.com>
2025-03-05 15:43:47 +0200

    Separate main_content

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from dash import callback_context, dcc, html, Input, Output, State, ALL
 from dash.exceptions import PreventUpdate
 
 from src import generate_form, paper_crf, arc, bridge_modals, index
+from src.bridge_main import MainContent
 
 pd.options.mode.copy_on_write = True
 
@@ -268,96 +269,6 @@ tree_column = dbc.Fade(
     }
 )
 
-column_defs = [{'headerName': "Question", 'field': "Question", 'wrapText': True},
-               {'headerName': "Answer Options", 'field': "Answer Options", 'wrapText': True}]
-
-row_data = [{'question': "", 'options': ""},
-            {'question': "", 'options': ""}]
-
-grid = html.Div(
-    dag.AgGrid(
-        id='CRF_representation_grid',
-        columnDefs=column_defs,
-        rowData=row_data,
-        defaultColDef={"sortable": True, "filter": True, 'resizable': True},
-        columnSize="sizeToFit",
-        dashGridOptions={
-            "rowDragManaged": True,
-            "rowDragEntireRow": True,
-            "rowDragMultiRow": True, "rowSelection": "multiple",
-            "suppressMoveWhenRowDragging": True,
-            "autoHeight": True
-        },
-        rowClassRules={
-            "form-separator-row ": 'params.data.SeparatorType == "form"',
-            'section-separator-row': 'params.data.SeparatorType == "section"',
-        },
-        style={
-            'overflow-y': 'auto',  # Vertical scrollbar when needed
-            'height': '99%',  # Fixed height
-            'width': '100%',  # Fixed width, or you can specify a value in px
-            'white-space': 'normal',  # Allow text to wrap
-            'overflow-x': 'hidden',  # Hide overflowed content
-
-        }
-
-    ),
-    style={
-        'overflow-y': 'auto',  # Vertical scrollbar when needed
-        'height': '75vh',  # Fixed height
-        'width': '100%',  # Fixed width, or you can specify a value in px
-        'white-space': 'normal',  # Allow text to wrap
-        'overflow-x': 'hidden',  # Hide overflowed content
-        'text-overflow': 'ellipsis'  # Indicate more content with an ellipsis
-
-    }
-)
-
-# Main Content
-main_content = dbc.Container(
-    [
-        dbc.Row(
-            [
-                dbc.Col([html.Div(),
-
-                         # tree_items,
-
-                         html.Div(id='output-expanded', style={'display': 'none'})
-                         ]
-
-                        , width=5),  # 45% width
-                dbc.Col(
-                    [
-                        dbc.Row([html.Div(),
-                                 grid]),  # 90% height
-                        dbc.Row(
-                            [
-                                dbc.Col(dbc.Input(placeholder="CRF Name", type="text", id='crf_name')),
-                                dbc.Col(dbc.Button("Generate", color="primary", id='crf_generate'))
-                            ],
-                            style={"height": "10%"}  # Remaining height for input and button
-                        ),
-                        dbc.Row(html.Div([
-                            "BRIDGE is being developed by ISARIC. For inquiries, support, or collaboration, please write to: ",
-                            html.A("data@isaric.org", href="mailto:data@isaric.org"), ". ",
-                            "Licensed under a ",
-                            html.A("Creative Commons Attribution-ShareAlike 4.0 ",
-                                   href="https://creativecommons.org/licenses/by-sa/4.0/",
-                                   target="_blank"),
-                            "International License by ",
-                            html.A("ISARIC", href="https://isaric.org/", target="_blank"),
-                            " on behalf of Oxford University."]))
-                    ],
-                    width=7  # 55% width
-                )
-            ]
-        )
-    ],
-    fluid=True,
-    style={"margin-top": "4rem", "margin-left": "4rem", "z-index": 1, "width": "90vw"}
-    # Adjust margin to accommodate navbar and sidebar
-)
-
 app.layout = html.Div(
     [
         dcc.Store(id='show-Take a Look at Our Other Tools', data=True),  # Store to manage which page to display
@@ -370,14 +281,7 @@ app.layout = html.Div(
 
         dcc.Store(id='templates_checks_ready', data=False),
 
-        # navbar,
-        # sidebar,
-        # settings_column,
-        # preset_column,
-        # tree_column,
-        # main_content,
         dcc.Location(id='url', refresh=False),
-        # html.H1(id='titleURL'),
         html.Div(id='page-content'),
         dcc.Download(id="download-dataframe-csv"),
         dcc.Download(id='download-compGuide-pdf'),
@@ -403,7 +307,7 @@ def main_app():
         settings_column,
         preset_column,
         tree_column,
-        main_content,
+        MainContent.main_content,
     ])
 
 
---
+++ src/bridge_main.py
@@ -0,0 +1,102 @@
+import dash_ag_grid as dag
+import dash_bootstrap_components as dbc
+import pandas as pd
+from dash import html
+
+pd.options.mode.copy_on_write = True
+
+assets_dir = '../assets'
+icons_dir = f'{assets_dir}/icons'
+logos_dir = f'{assets_dir}/logos'
+screenshots_dir = f'{assets_dir}/screenshots'
+
+
+class MainContent:
+    column_defs = [{'headerName': "Question", 'field': "Question", 'wrapText': True},
+                   {'headerName': "Answer Options", 'field': "Answer Options", 'wrapText': True}]
+
+    row_data = [{'question': "", 'options': ""},
+                {'question': "", 'options': ""}]
+
+    grid = html.Div(
+        dag.AgGrid(
+            id='CRF_representation_grid',
+            columnDefs=column_defs,
+            rowData=row_data,
+            defaultColDef={"sortable": True, "filter": True, 'resizable': True},
+            columnSize="sizeToFit",
+            dashGridOptions={
+                "rowDragManaged": True,
+                "rowDragEntireRow": True,
+                "rowDragMultiRow": True, "rowSelection": "multiple",
+                "suppressMoveWhenRowDragging": True,
+                "autoHeight": True
+            },
+            rowClassRules={
+                "form-separator-row ": 'params.data.SeparatorType == "form"',
+                'section-separator-row': 'params.data.SeparatorType == "section"',
+            },
+            style={
+                'overflow-y': 'auto',  # Vertical scrollbar when needed
+                'height': '99%',  # Fixed height
+                'width': '100%',  # Fixed width, or you can specify a value in px
+                'white-space': 'normal',  # Allow text to wrap
+                'overflow-x': 'hidden',  # Hide overflowed content
+
+            }
+
+        ),
+        style={
+            'overflow-y': 'auto',  # Vertical scrollbar when needed
+            'height': '75vh',  # Fixed height
+            'width': '100%',  # Fixed width, or you can specify a value in px
+            'white-space': 'normal',  # Allow text to wrap
+            'overflow-x': 'hidden',  # Hide overflowed content
+            'text-overflow': 'ellipsis'  # Indicate more content with an ellipsis
+
+        }
+    )
+
+    main_content = dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col([html.Div(),
+
+                             # tree_items,
+
+                             html.Div(id='output-expanded', style={'display': 'none'})
+                             ]
+
+                            , width=5),  # 45% width
+                    dbc.Col(
+                        [
+                            dbc.Row([html.Div(),
+                                     grid]),  # 90% height
+                            dbc.Row(
+                                [
+                                    dbc.Col(dbc.Input(placeholder="CRF Name", type="text", id='crf_name')),
+                                    dbc.Col(dbc.Button("Generate", color="primary", id='crf_generate'))
+                                ],
+                                style={"height": "10%"}  # Remaining height for input and button
+                            ),
+                            dbc.Row(html.Div([
+                                "BRIDGE is being developed by ISARIC. For inquiries, support, or collaboration, please write to: ",
+                                html.A("data@isaric.org", href="mailto:data@isaric.org"), ". ",
+                                "Licensed under a ",
+                                html.A("Creative Commons Attribution-ShareAlike 4.0 ",
+                                       href="https://creativecommons.org/licenses/by-sa/4.0/",
+                                       target="_blank"),
+                                "International License by ",
+                                html.A("ISARIC", href="https://isaric.org/", target="_blank"),
+                                " on behalf of Oxford University."]))
+                        ],
+                        width=7  # 55% width
+                    )
+                ]
+            )
+        ],
+        fluid=True,
+        style={"margin-top": "4rem", "margin-left": "4rem", "z-index": 1, "width": "90vw"}
+        # Adjust margin to accommodate navbar and sidebar
+    )
