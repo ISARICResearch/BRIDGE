116f3682959769d3dd13130e711e78bd2139d781
Laura Thomson <lrthmsn@gmail.com>
2025-03-12 10:16:45 +0200

    Refactoring

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import json
 import re
 from datetime import datetime
 from os.path import join, abspath, dirname
-from urllib.parse import parse_qs, urlparse
 
 import dash
 import dash_ag_grid as dag
 import dash_bootstrap_components as dbc
 import dash_treeview_antd
 import pandas as pd
-from dash import callback_context, dcc, html, Input, Output, State, ALL
+from dash import callback_context, dcc, html, Input, Output, State
 from dash.exceptions import PreventUpdate
 
 from src import generate_form, paper_crf, arc, bridge_modals, index
@@ -28,8 +27,6 @@ app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.BOOTSTRAP, 'https://use.fontawesome.com/releases/v5.8.1/css/all.css'],
                 suppress_callback_exceptions=True)
 app.title = 'BRIDGE'
-
-app = SideBar.add_sidebar(app)
 
 modified_list = []
 
@@ -99,6 +96,9 @@ app.layout = html.Div(
     ]
 )
 
+app = SideBar.add_sidebar(app)
+# app = BridgeCrf(grouped_presets).add_callbacks(app)
+
 
 def main_app():
     return html.Div([
@@ -127,45 +127,6 @@ def start_app(n_clicks):
         return '/'
     else:
         return '/main'
-
-
-####################
-# get URL parameter
-####################
-@app.callback(
-    [Output('crf_name', 'value'), Output({'type': 'template_check', 'index': ALL}, 'value')],
-    [Input('templates_checks_ready', 'data')],
-    [State('url', 'href')],
-    prevent_initial_call=True,
-)
-def update_output_based_on_url(template_check_flag, href):
-    if not template_check_flag:
-        return dash.no_update
-
-    if href is None:
-        return [''] + [[] for _ in grouped_presets.keys()]
-
-    if '?param=' in href:
-        # Parse the URL to extract the parameters
-        parsed_url = urlparse(href)
-        params = parse_qs(parsed_url.query)
-
-        # Accessing the 'param' parameter
-        param_value = params.get('param', [''])[0]  # Default to an empty string if the parameter is not present
-
-        # Example: Split param_value by underscore
-        group, value = param_value.split('_') if '_' in param_value else (None, None)
-
-        # Prepare the outputs
-        checklist_values = {key: [] for key in grouped_presets.keys()}
-
-        if group in grouped_presets and value in grouped_presets[group]:
-            checklist_values[group] = [value]
-
-        # Return the value for 'crf_name' and checklist values
-        return [value], [checklist_values[key] for key in grouped_presets.keys()]
-    else:
-        return dash.no_update
 
 
 #################################
