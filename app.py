ea98c25e88f6a00e4a7a1eda9f1d57668f0d77be
Laura Thomson <lrthmsn@gmail.com>
2025-08-07 13:57:08 +0100

    Increase delay_show=750 to prevent screen flash

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def main_app():
                 TreeItems(TREE_ITEMS_DATA).tree_column,
                 MainContent.main_content,
             ],
-            delay_show=500,
+            delay_show=750,
         ),
     ])
 
