b21e6b62989066aff2f2de07d99061ded2ede9e5
Laura Thomson <lrthmsn@gmail.com>
2025-09-16 14:58:50 +0100

    Fix TODOs

--- tests/test_save.py
+++ tests/test_save.py
@@ -75,8 +75,10 @@ def test_on_save_click_no_action(n_clicks,
     assert output == expected_output
 
 
-@mock.patch('bridge.callbacks.save.datetime', return_value=datetime(2025, 9, 30))
-def test_on_save_click(mock_date):
+@mock.patch('bridge.callbacks.save.datetime')
+@mock.patch('bridge.callbacks.save.get_trigger_id', return_value='crf_save')
+@mock.patch('bridge.callbacks.save.get_crf_name', return_value='test_crf')
+def test_on_save_click(mock_crf_name, mock_trigger_id, mock_date):
     mock_date.today.return_value = datetime(2025, 9, 30)
     mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
 
@@ -86,28 +88,16 @@ def test_on_save_click(mock_date):
     current_datadicc_saved = '{"columns":["Form","Section","Variable"], "index":[0,1,2], "data":[["presentation",null,"subjid"],["presentation","INCLUSION CRITERIA","inclu_disease"],["presentation","ONSET & PRESENTATION","pres_firstsym"]]}'
     selected_version_data = {'selected_version': 'v1.1.2'}
     selected_language_data = {'selected_language': 'English'}
-    crf_name = 'test'
+    crf_name = 'test_crf'
     ulist_variable_choices_saved = '[["inclu_disease", [[1, "Adenovirus", 1], [2, "Andes virus infection (hantavirus)", 0]]]]'
     multilist_variable_choices_saved = '[["pres_firstsym", [[1, "Abdominal pain", 1], [2, "Abnormal weight loss", 0]]]]'
 
     def run_callback(trigger, n_clicks, checked_variables, current_datadicc_saved, selected_version_data,
                      selected_language_data, crf_name, ulist_variable_choices_saved, multilist_variable_choices_saved):
         context_value.set(AttributeDict(**{"triggered_inputs": [trigger]}))
-
-        # TODO
-        # mocker.patch('bridge.utils.trigger_id.get_trigger_id', return_value='crf_save')
-        # mocker.patch('bridge.utils.crf_name.get_crf_name', return_value='test')
-
-        # with mock.patch('bridge.callbacks.save.datetime') as mock_date:
         return save.on_save_click(n_clicks, checked_variables, current_datadicc_saved, selected_version_data,
                                   selected_language_data, crf_name, ulist_variable_choices_saved,
                                   multilist_variable_choices_saved)
-
-    # TODO: Test these
-    # TODO: Need to mock other functions as well
-    # mocker.patch('bridge.utils.trigger_id.get_trigger_id', return_value='crf_save')
-    # mocker.patch('bridge.utils.crf_name.get_crf_name', return_value='test')
-    # get_checked_data_for_list
 
     ctx = copy_context()
     output = ctx.run(
