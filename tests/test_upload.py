1d356d56284e8a2d139ffd013532b5ab6ea84ceb
Laura Thomson <lrthmsn@gmail.com>
2025-09-17 16:57:07 +0100

    Add unit tests

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,9 +1,12 @@
+import json
 from contextvars import copy_context
 from unittest import mock
 
 import dash
 import pandas as pd
 import pytest
+from dash._callback_context import context_value
+from dash._utils import AttributeDict
 from pandas.testing import assert_frame_equal
 
 import bridge.callbacks.upload as upload
@@ -18,7 +21,6 @@ import bridge.callbacks.upload as upload
     ]
 )
 def test_on_upload_crf(upload_filename, upload_contents, expected_output):
-
     def run_callback(filename, contents):
         return upload.on_upload_crf(filename, contents)
 
@@ -78,3 +80,115 @@ def test_update_for_upload_list_selected(mock_get_translations):
         '[[1, "Afghanistan", 0], [2, "Aland Islands", 1], "88, Other"]]]')
 
     assert list_variable_choices_updated == list_variable_choices_expected
+
+
+def test_load_upload_arc_version_language_not_triggered():
+    trigger = None
+    upload_version_data = None
+    upload_language_data = None
+    selected_version_data = None
+    selected_language_data = None
+
+    output = get_output_load_upload_arc_version_language(trigger, upload_version_data, upload_language_data,
+                                                         selected_version_data, selected_language_data)
+
+    assert output == (dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update,
+                      False, None, dash.no_update, dash.no_update)
+
+
+@pytest.fixture
+def triggered_trigger():
+    trigger = [{'prop_id': 'upload-version-store.data', 'value': {'upload_version': 'v1.1.2'}}, {
+        'prop_id': 'upload-language-store.data', 'value': {'upload_language': 'English'}}]
+    return trigger
+
+
+def test_load_upload_arc_version_language_no_update(triggered_trigger):
+    upload_version_data = {'upload_version': 'v1.1.2'}
+    upload_language_data = {'upload_language': 'English'}
+    selected_version_data = {'selected_version': 'v1.1.2'}
+    selected_language_data = {'selected_language': 'English'}
+
+    output = get_output_load_upload_arc_version_language(triggered_trigger, upload_version_data, upload_language_data,
+                                                         selected_version_data, selected_language_data)
+
+    assert output == (dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update,
+                      True, None, dash.no_update, dash.no_update)
+
+
+@mock.patch('bridge.callbacks.upload.logger')
+@mock.patch('bridge.callbacks.upload.Language.get_version_language_related_data')
+def test_load_upload_arc_version_language_json_error(mock_error, mock_logger, triggered_trigger):
+    mock_error.return_value = (None, None, None, None, None, None)
+    mock_error.side_effect = json.JSONDecodeError('msg', '', 1)
+
+    upload_version_data = {'upload_version': 'v1.1.2'}
+    upload_language_data = {'upload_language': 'English'}
+    selected_version_data = {'selected_version': 'v1.1.3'}
+    selected_language_data = {'selected_language': 'English'}
+
+    output = get_output_load_upload_arc_version_language(triggered_trigger, upload_version_data, upload_language_data,
+                                                         selected_version_data, selected_language_data)
+
+    assert output == (dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update,
+                      False, None, dash.no_update, dash.no_update)
+
+
+@mock.patch('bridge.callbacks.upload.logger')
+@mock.patch('bridge.callbacks.upload.Language.get_version_language_related_data')
+def test_load_upload_arc_version_language(mock_error, mock_logger, triggered_trigger):
+    data = {
+        'Form': ['presentation', 'presentation'],
+        'Section': ['INCLUSION CRITERIA', 'DEMOGRAPHICS'],
+        'Variable': ['inclu_disease', 'demog_country'],
+    }
+    df_upload_version = pd.DataFrame.from_dict(data)
+    version_commit = 'abc123'
+    version_grouped_presets = {'ARChetype Disease CRF': ['Covid', 'Dengue', 'Mpox', 'H5Nx'],
+                               'ARChetype Syndromic CRF': ['ARI'], 'Recommended Outcomes': ['Dengue'],
+                               'Score': ['CharlsonCI'], 'UserGenerated': ['Oropouche']}
+    version_accordion_items = ['Some accordian items']
+    version_ulist_variable_choices = 'Some ulist variable choices'
+    version_multilist_variable_choices = 'Some multilist variable choices'
+
+    mock_error.return_value = (df_upload_version, version_commit, version_grouped_presets, version_accordion_items,
+                               version_ulist_variable_choices, version_multilist_variable_choices)
+
+    upload_version_data = {'upload_version': 'v1.1.2'}
+    upload_language_data = {'upload_language': 'Finnish'}
+    selected_version_data = {'selected_version': 'v1.1.3'}
+    selected_language_data = {'selected_language': 'English'}
+
+    output = get_output_load_upload_arc_version_language(triggered_trigger, upload_version_data, upload_language_data,
+                                                         selected_version_data, selected_language_data)
+
+    assert output == (
+        {'selected_version': 'v1.1.2'},
+        {'selected_language': 'Finnish'},
+        {'commit': version_commit},
+        version_accordion_items,
+        version_grouped_presets,
+        df_upload_version.to_json(date_format='iso', orient='split'),
+        True,
+        None,
+        version_ulist_variable_choices,
+        version_multilist_variable_choices,
+    )
+
+
+def get_output_load_upload_arc_version_language(trigger, upload_version_dict, upload_language_dict,
+                                                selected_version_dict, selected_language_dict):
+    def run_callback(upload_version_data, upload_language_data, selected_version_data, selected_language_data):
+        context_value.set(AttributeDict(**{"triggered_inputs": trigger}))
+        return upload.load_upload_arc_version_language(upload_version_data, upload_language_data, selected_version_data,
+                                                       selected_language_data)
+
+    ctx = copy_context()
+    output = ctx.run(
+        run_callback,
+        upload_version_dict,
+        upload_language_dict,
+        selected_version_dict,
+        selected_language_dict
+    )
+    return output
