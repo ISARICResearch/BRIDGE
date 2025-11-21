258b3c19fc1e5135ac2a399601959d988404f672
Laura Thomson <lrthmsn@gmail.com>
2025-11-21 15:43:48 +0000

    Add unit tests

--- tests/callbacks/test_url.py
+++ tests/callbacks/test_url.py
@@ -27,6 +27,12 @@ def test_update_output_based_on_url_no_action(template_check, presets, address, 
                 'http://127.0.0.1:8050/main?param=ARChetype%20Disease%20CRF_Dengue',
                 (['Dengue'], [['Dengue'], []]),
         ),
+        (
+                True,
+                {'ARChetype Disease CRF': ['Covid', 'Dengue', 'Mpox'], 'Recommended Outcomes': ['Dengue']},
+                'http://127.0.0.1:8050/main?param=Recommended%20Outcomes_Dengue',
+                (['Dengue'], [[], ['Dengue']]),
+        ),
     ]
 )
 def test_update_output_based_on_url(template_check, presets, address, expected_output):
