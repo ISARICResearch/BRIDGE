f48cb9279ab671d31aca29a6686e3e6e521d39e4
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2026-02-26 19:12:55 +0000

    remove outdated imports

--- tests/callbacks/test_grid.py
+++ tests/callbacks/test_grid.py
@@ -4,7 +4,7 @@ from unittest import mock
 import numpy as np
 import pandas as pd
 import pytest
-from statsmodels.compat.pandas import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 from bridge.callbacks import grid
 
