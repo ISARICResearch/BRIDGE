37a836f24139e032f7777dd6c512be2465070a7b
Sandeep Murthy <Sandeep.Murthy@ndm.ox.ac.uk>
2026-06-29 14:47:41 +0100

    fix: CRF preset name utils functions and tests

--- bridge/utils/crf.py
+++ bridge/utils/crf.py
@@ -1,19 +1,77 @@
+__all__ = [
+    "get_crf_name",
+    "get_selected_crf_presets",
+]
+
+
+# -- IMPORTS --
+
+# -- Standard libraries --
 from bridge.utils.logger import setup_logger
 
 logger = setup_logger(__name__)
 
 
-def get_crf_name(crf_name, checked_values):
-    if crf_name is not None:
+def get_selected_crf_presets(
+    grouped_presets: dict[str, list[str]], checked_values: list[bool]
+) -> tuple[tuple[str, str]]:
+    """:py:class:`tuple` : A tuple of selected CRF presets organised by section name.
+
+    Parameters
+    ----------
+    grouped_presets : dict
+        A dict of CRF presets/templates keyed by section name.
+
+    checked_values : list
+        A list of boolean indicators of CRF preset checked values / selectors.
+
+    Returns
+    -------
+    tuple
+        A tuple of tuples composed of section name and selected CRF preset name.
+    """
+    flattened_grouped_presets = [
+        (k, v) for k, values in grouped_presets.items() for v in values
+    ]
+
+    return tuple(
+        preset
+        for preset, preset_checked in zip(flattened_grouped_presets, checked_values)
+        if preset_checked
+    )
+
+
+def get_crf_name(
+    crf_name: str | list | None,
+    checked_values: list[bool],
+    grouped_presets: dict[str, list[str]] | None = None,
+) -> str:
+    """:py:class:`str` : The name of a selected CRF preset.
+
+    Parameters
+    ----------
+    crf_name : str
+        The CRF preset name or list of names, which could be null, to check
+        against.
+
+    checked_values : list
+        A list of bools indicating CRF preset selections/checks - this will be
+        an ordered list as long as the total number of CRF preset options in
+        all sections.
+
+    grouped_presets : dict
+        A dict of CRF preset names keyed by section name.
+
+    Returns
+    -------
+    str
+        The selected CRF preset name.
+    """
+    if crf_name:
         if isinstance(crf_name, list):
             crf_name = crf_name[0]
     else:
-        extracted_text = [
-            item for sublist in checked_values for item in sublist if item
-        ]
-        if len(extracted_text) > 0:
-            crf_name = extracted_text[0]
-    if not crf_name:
-        crf_name = "no_name"
+        crf_name = get_selected_crf_presets(grouped_presets, checked_values)[0][1]
     logger.info(f"crf_name: {crf_name}")
+
     return crf_name
--- tests/utils/test_crf.py
+++ tests/utils/test_crf.py
@@ -3,29 +3,110 @@ from unittest import mock
 
 import pytest
 
-from bridge.utils.crf import get_crf_name
+from bridge.utils.crf import get_selected_crf_presets, get_crf_name
 
 
 @pytest.mark.parametrize(
-    "name, checked, expected_output",
+    "grouped_presets, checked_values, expected_output",
     [
-        (["name1", "name2", "name3"], [], "name1"),
-        (None, [], "no_name"),
-        (None, [["Covid"], [], [], [], []], "Covid"),
-        (None, [["Covid", "Dengue"], [], [], [], []], "Covid"),
-        (None, [["Dengue"], [], ["Oropouche"], [], []], "Dengue"),
-        (None, [[], [], ["Oropouche"], [], []], "Oropouche"),
+        (
+            {
+                "test_section1": [
+                    "test_option1__first_selected",
+                    "test_option2",
+                    "test_option3",
+                ],
+                "test_section2": ["test_option4", "test_option5", "test_option6"],
+            },
+            [True, False, False, False, False, False],
+            (("test_section1", "test_option1__first_selected"),),
+        ),
+        (
+            {
+                "test_section1": [
+                    "test_option1__first_selected",
+                    "test_option2",
+                    "test_option3__second_selected",
+                ],
+                "test_section2": ["test_option4", "test_option5", "test_option6"],
+            },
+            [True, False, True, False, False, False],
+            (
+                ("test_section1", "test_option1__first_selected"),
+                ("test_section1", "test_option3__second_selected"),
+            ),
+        ),
+        (
+            {
+                "test_section1": [
+                    "test_option1",
+                    "test_option2__first_selected",
+                    "test_option3",
+                ],
+                "test_section2": [
+                    "test_option4",
+                    "test_option5__second_selected",
+                    "test_option6",
+                ],
+            },
+            [False, True, False, False, True, False],
+            (
+                ("test_section1", "test_option2__first_selected"),
+                ("test_section2", "test_option5__second_selected"),
+            ),
+        ),
     ],
 )
 @mock.patch("bridge.utils.crf.logger")
-def test_get_crf_name(_mock_logger, name, checked, expected_output):
-    def run_callback(crf_name, checked_values):
-        return get_crf_name(crf_name, checked_values)
+def test_get_selected_crf_presets(
+    _mock_logger, grouped_presets, checked_values, expected_output
+):
+    received_output = get_selected_crf_presets(grouped_presets, checked_values)
+
+    assert expected_output == received_output
+
+
+@pytest.mark.parametrize(
+    "name, checked, grouped_presets, expected_output",
+    [
+        (["name1", "name2", "name3"], [], None, "name1"),
+        (
+            None,
+            [True, False, False, False, False, False],
+            {
+                "test_section1": [
+                    "test_option1__first_selected",
+                    "test_option2",
+                    "test_option3",
+                ],
+                "test_section2": ["test_option4", "test_option5", "test_option6"],
+            },
+            "test_option1__first_selected",
+        ),
+        (
+            None,
+            [True, False, False, True, False, False],
+            {
+                "test_section1": [
+                    "test_option1__first_selected",
+                    "test_option2",
+                    "test_option3",
+                ],
+                "test_section2": [
+                    "test_option4__first_selected",
+                    "test_option5",
+                    "test_option6",
+                ],
+            },
+            "test_option1__first_selected",
+        ),
+    ],
+)
+@mock.patch("bridge.utils.crf.logger")
+def test_get_crf_name(_mock_logger, name, checked, grouped_presets, expected_output):
+    def run_callback(crf_name, checked_values, grouped_presets):
+        return get_crf_name(crf_name, checked_values, grouped_presets=grouped_presets)
 
     ctx = copy_context()
-    output = ctx.run(
-        run_callback,
-        name,
-        checked,
-    )
+    output = ctx.run(run_callback, name, checked, grouped_presets=grouped_presets)
     assert output == expected_output
